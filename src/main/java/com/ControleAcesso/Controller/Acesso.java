c269b0f533909d691cc19481d68005145c55e74e
Josias <josiassilva32@gmail.com>
2019-04-23 07:49:46 -0300

    inicio

---
+++ src/main/java/com/ControleAcesso/Controller/Acesso.java
@@ -0,0 +1,21 @@
+package com.ControleAcesso.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ControleAcesso.Service.AcessoService;
+
+@Controller
+@RequestMapping(
+		value="/login",
+		consumes=MediaType.APPLICATION_JSON_UTF8_VALUE,
+		produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class Acesso {
+	
+    @Autowired
+	AcessoService acessoService;
+	
+
+}
---
+++ src/main/java/com/ControleAcesso/Model/Usuario.java
@@ -0,0 +1,68 @@
+package com.ControleAcesso.Model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Usuario implements Serializable {
+	
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="usuario")
+	private String usuario;
+	
+	@Column(name="senha")
+	private String senha;
+
+	public Integer getId() {
+		return id;
+	}
+
+	
+	public Usuario() {
+		super();
+		
+	}
+	
+	public Usuario(Integer id) {
+		super();
+		this.setId(id);
+		
+	}
+	
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
